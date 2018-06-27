c81a7ec20ffd52496d0d5c39b5cbbfed93f5710f
ConnorCairns <connor@connorcairns.xyz>
2018-06-27 13:08:48 +0100

    nice for loop

--- app.py
+++ app.py
@@ -20,8 +20,13 @@ def contact():
 
 @app.route('/double-exposure/')
 def double_exposure():
-    return render_template('double-exposure.html')
+    x = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]
+    return render_template('double-exposure.html', x=x, enumerate=enumerate)
+
+
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
+
