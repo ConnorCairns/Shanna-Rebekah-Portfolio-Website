3329b159aa546f7bd5d9d9b95e7bb919089ce6fc
ConnorCairns <connor@connorcairns.xyz>
2018-08-22 14:59:03 +0100

    login/register

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import Registration, Login
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'ihatecoursework'
 
 @app.route('/')
 def index():
@@ -23,6 +24,24 @@ def double_exposure():
     x = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]
     return render_template('double-exposure.html', x=x, enumerate=enumerate)
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = Registration()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = Login()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login failed', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 
