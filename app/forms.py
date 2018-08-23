2a2fc7fa857a3e14bc05d5a254681f70292463e6
ConnorCairns <connor@connorcairns.xyz>
2018-08-23 14:05:37 +0100

    login works

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, length, Email, EqualTo
+from wtforms.validators import DataRequired, length, Email, EqualTo, ValidationError
+from app.models import User
 
 class Registration(FlaskForm):
     username = StringField("Username", validators=[DataRequired(),length(min=2, max=20)])
@@ -8,6 +9,16 @@ class Registration(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     password_confirm = PasswordField("Confirm Password", validators=[DataRequired(),EqualTo("password")])
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username taken')
+    
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('Email already has an account created')
 
 class Login(FlaskForm):
     email = StringField("Email", validators=[DataRequired(),Email()])
