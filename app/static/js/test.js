e63dcf51b6a256b34fedb6c38df85af3d9233893
ConnorCairns <connor@connorcairns.xyz>
2018-10-23 18:58:18 +0100

    merge sort

--- app/static/js/test.js
+++ app/static/js/test.js
@@ -27,7 +27,6 @@ class DropDown extends React.Component {
                 <button className="btn btn-secondary dropdown-toggle float-right mb-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Order by</button>
                 <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
                     <a className="dropdown-item" onClick={this.props.handleclick} id="alphabetical">Alphabetical</a>
-                    <a className="dropdown-item" onClick={this.props.handleclick} id="last_login">Last Login</a>
                     <a className="dropdown-item" onClick={this.props.handleclick} id="id">ID</a>
                 </div>
             </div>
@@ -39,6 +38,34 @@ class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = { show: "dashboard", dropdown: "id" }
+    }
+
+    mergeSort(array) {
+        if (array.length === 1) {
+            return array
+        }
+        let middle = Math.floor(array.length / 2)
+        let left = array.slice(0, middle)
+        let right = array.slice(middle, array.length)
+
+        return this.merge(this.mergeSort(left), this.mergeSort(right))
+    }
+
+    merge(left, right) {
+        let result = []
+        let leftIndex = 0
+        let rightIndex = 0
+
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] < right[rightIndex]) {
+                result.push(left[leftIndex])
+                leftIndex++
+            } else {
+                result.push(right[rightIndex])
+                rightIndex++
+            }
+        }
+        return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
     }
 
     tableHead() {
@@ -63,19 +90,44 @@ class Dashboard extends React.Component {
         }
     }
 
-    tableBody() {
+    getUsersArray() {
         let zip = (array1, array2) => array1.map((x, i) => [x, array2[i]]);
-        let new_users = zip(users, emails);
-        let new_photos = zip(photo_name, photo_category);
+        let zip_users = zip(users, emails);
+        let zip_photos = zip(photo_name, photo_category);
+        for (var i = 0; i < zip_users.length; i++) {
+            zip_users[i].push(last_login[i])
+        }
+        for (var i = 0; i < zip_photos.length; i++) {
+            zip_photos[i].push(clients[i])
+        }
+        if (this.state.dropdown === "alphabetical") {
+            if (this.state.show === "users") {
+                let new_users = this.mergeSort(zip_users)
+                return new_users
+            } else if (this.state.show === "photos") {
+                let new_photos = this.mergeSort(zip_photos)
+                return new_photos
+            }
+        } else if (this.state.dropdown === "id") {
+            if (this.state.show === "users") {
+                return zip_users
+            } else if (this.state.show === "photos") {
+                return zip_photos
+            }
+        }
+    }
+
+    tableBody() {
+        let array = this.getUsersArray()
         if (this.state.show === "users") {
             return (
                 <tbody>
-                    {new_users.map((user, i) => (
+                    {array.map((user, i) => (
                         <tr>
                             <th style={{ width: '2%' }} scope="row">{i + 1}</th>
                             <td>{user[0]}</td>
                             <td>{user[1]}</td>
-                            <td>{last_login[i]}</td>
+                            <td>{user[2]}</td>
                         </tr>
                     ))}
                 </tbody>
@@ -83,12 +135,12 @@ class Dashboard extends React.Component {
         } else if (this.state.show === "photos") {
             return (
                 <tbody>
-                    {new_photos.map((photo, i) => (
+                    {array.map((photo, i) => (
                         <tr>
                             <th style={{ width: '2%' }} scope="row">{i + 1}</th>
                             <td>{photo[0]}</td>
                             <td>{photo[1]}</td>
-                            <td>{clients[i]}</td>
+                            <td>{photo[2]}</td>
                         </tr>
                     ))}
                 </tbody>
@@ -107,14 +159,6 @@ class Dashboard extends React.Component {
         )
     }
 
-    dashboard() {
-        return (
-            <div>
-                <p>haha yes</p>
-            </div>
-        )
-    }
-
     test() {
         if (this.state.show === "users" || this.state.show === "photos") {
             return (
@@ -122,10 +166,6 @@ class Dashboard extends React.Component {
                     <DropDown handleclick={this.handleDropdown} />
                     {this.makeTable()}
                 </div>
-            )
-        } else if (this.state.show === "dashboard") {
-            return (
-                this.dashboard()
             )
         }
     }
@@ -135,9 +175,9 @@ class Dashboard extends React.Component {
             <div>
                 <div className="row text-center align-items-center mb-3">
                     <div className="col-12">
-                        <h1>Haha yes</h1>
+                        <h1>Admin Page</h1>
                         <hr className="mt-1 mb-4 bg-secondary"></hr>
-                        <p className="font-weight-normal text-justify text-center">Admin things</p>
+                        <p className="font-weight-normal text-justify text-center">Welcome to the admin page!<br></br>Use the navigation buttons to view information</p>
                         <Button handleclick={this.handleButton} />
                     </div>
                 </div>
